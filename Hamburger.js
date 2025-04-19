fcd2c35d87312d83e68d2260e139b7b40abaf63d
FDGBOY <129415117+FDGBOY@users.noreply.github.com>
2025-04-20 00:59:32 +0700

    Add files via upload

---
+++ Hamburger.js
@@ -0,0 +1,9 @@
+// filepath: c:\Users\User\Documents\WEB_BOY\project_WEB\script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
